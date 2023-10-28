f1626fc46495f4ca064ae9547c15f7e4560cb7c1
ChatelardDaniel <cdanasc@hotmail.fr>
2023-10-28 17:31:37 +0200

    ajout

---
+++ Formulaire dynamique avec JS/scripts/evenements.js
@@ -0,0 +1,7 @@
+import Formulaire from "./formulaire";
+
+// Créer le formulaire
+const formulaire = new Formulaire('formulaire');
+
+formulaire.maskChamp('societe');
+formulaire.maskChamp('email');
